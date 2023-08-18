9df5143eafeef569286038fd2578be41444cb88e
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-08-18 20:41:44 +0300

    Реализация штатного сотрудника Staff.cpp

--- Office/Staff.cpp
+++ Office/Staff.cpp
@@ -1,1 +1,24 @@
 #include "Staff.h"
+
+Staff::Staff()
+{
+	bonus = 0;
+}
+
+Staff::Staff(string name, string position, float salary, float bonus):Employee(name,position,salary)
+{
+	this->bonus = bonus;
+}
+
+void Staff::show() const
+{
+	cout << "Name: " << name << endl;
+	cout << "Position: " << position << endl;
+	cout << "Salary: " << salary << endl;
+	cout << "Bonus: " << bonus << endl;
+}
+
+float Staff::calcSalary() const
+{
+	return salary+bonus;
+}
