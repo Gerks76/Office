c29ea26979a3fe3cb232069ddbc6edbb9cf829a5
Zolotopup Oleksandr <zolotopup76@gmail.com>
2023-08-18 20:35:02 +0300

    Штатный сотрудник Staff Staff.h

---
+++ Office/Staff.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Employee.h"
+
+class Staff :
+    public Employee
+{
+protected:
+    float bonus;
+public:
+    Staff();
+    Staff(string name, string position, float salary, float bonus);
+
+    void show()const override;
+    float calcSalary()const override;
+};
+
